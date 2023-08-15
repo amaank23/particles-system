0d0882f63921b09f18dd1e7598880e8533540929
Aman <amanwaseem24@gmail.com>
2023-08-15 12:53:19 +0500

    created a boundry for particles to bound around

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Particle {
-    constructor(effect){
-        this.effect = effect;
-        this.x = Math.random() * this.effect.width;
-        this.y = Math.random() * this.effect.height;
-        this.radius = 15
-    }
-    draw(context){
-       context.beginPath();
-       context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-       context.fill() 
-    }
+  constructor(effect) {
+    this.effect = effect;
+    this.radius = 15;
+    this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
+    this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
+  }
+  draw(context) {
+    context.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke()
+  }
+  update(){
+    this.x += 1;
+  }
 }
-
 
 class Effect {
-    constructor(canvas){
-        this.canvas = canvas;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.particles = [];
-        this.noOfParticles = 20;
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.noOfParticles = 200;
+    this.createParticles();
+  }
+  createParticles() {
+    for (let i = 0; i < this.noOfParticles; i++) {
+      this.particles.push(new Particle(this));
     }
-    createParticles(){
-        for(let i = 0; i < this.noOfParticles; i++){
-            this.particles.push(new Particle(this))
-        }
-    }
+  }
+  handleParticles(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+      particle.update()
+    });
+  }
 }
 
+const effect = new Effect(canvas);
 
-function animate(){
 
-}
+effect.handleParticles(ctx);
